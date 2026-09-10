9c11c31e9581505e5a2f5a1e68d3b228a4236a8a
Victor Estevao <victormorekids@gmail.com>
2026-09-10 08:20:13 -0300

    commit atualizaçao musica nicel de permissão

--- cogs/musica.py
+++ cogs/musica.py
@@ -16,6 +16,11 @@ YTDL_OPTIONS = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0',
+    'extractor_args':{
+        'youtube': {
+            'player_client': ['ios', 'android']
+        }
+    }
 }
 if PROXY_URL:
     YTDL_OPTIONS['proxy'] = PROXY_URL
